9f8643deae38ef5cd13179b834b210831f3ae928
Arlwin <arlwinkarlfajardo@gmail.com>
2022-04-30 16:03:53 +0800

    ci: (staging) init workflow

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Build Vue
+cd frontend
+npm run build
+
+# Copy to Springboot
+cd ..
+cp -r frontend/dist/* backend/src/main/resources/static/.
+
+# Build gradle
+cd backend
+./gradlew build --no-daemon
